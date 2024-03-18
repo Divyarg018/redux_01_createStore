02a760ded100649d1a70edbb1870b3ae5a4e9afd
Divya R <kwrdivyaraj100@gmail.com>
2024-03-18 22:22:00 +0530

    Adding increment and decrement buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import './App.css';
+import { useSelector } from "react-redux";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
   return (
     <div >
-
+      <h1>Counter App</h1>
+      <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={Decreement}>Decrement</button>
     </div>
   );
 }
