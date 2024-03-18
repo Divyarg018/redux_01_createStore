af7d3e86f0a65b103ae5eb57bd044fce401d772b
Divya R <kwrdivyaraj100@gmail.com>
2024-03-18 22:36:43 +0530

    Counter app using createStore

--- src/store/index.js
+++ src/store/index.js
@@ -9,6 +9,9 @@ const reducerFn = (state = { counter: 10 }, action) => {
     if (action.type === "DEC") {
         return { counter: state.counter - 1 };
     }
+    if (action.type === "ADD") {
+        return { counter: state.counter + action.payload };
+    }
     return state;
 };
 
